cef80ec871cdbe78e40b0aa0a2aa8c375aa7ac8a
Dorin Dura <duradorin11@yahoo.com>
2024-08-05 12:26:16 +0300

    Download-Commit-Update

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -5,7 +5,7 @@ import { assets } from "../../assets/assets";
 const AppDownload = () => {
   return (
     <div className='app-download' id='app-download'>
-      <p>For Better Experience Download <br /> Tomato App</p>
+      <p>For Better Experience Download <br /> Feastly App</p>
       <div className="app-download-platforms">
         <img src={assets.play_store} alt="Play Store Image" />
         <img src={assets.app_store} alt="App Store Image" />
